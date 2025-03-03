f8b98b80c7e76d94fc19ac90ebbe46a6c95d27ab
mccierar <mccier.ar@gmx.com>
2025-03-03 14:13:53 +0000

    Add 1.ts

---
+++ src/1.ts
@@ -0,0 +1,8 @@
+const generateRandomTsCode = () => {
+  const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  let code = '';
+  for (let i = 0; i < 8; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+};
